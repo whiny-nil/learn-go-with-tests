bbb508adcce4b74b7953d19843730bd6bc0b2d80
Marc Kelsey <marc.kelsey@gmail.com>
2021-09-26 08:53:53 -0400

    Refactor tails sum

--- 04-arrays-and-slices/sums_test.go
+++ 04-arrays-and-slices/sums_test.go
@@ -26,10 +26,21 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	expected := []int{2, 0}
-	got := SumAllTails([]int{1, 2}, []int{9, 0})
+	t.Run("it adds the tails of given slices", func(t *testing.T) {
+		expected := []int{2, 0}
+		got := SumAllTails([]int{1, 2}, []int{9, 0})
 
-	if !reflect.DeepEqual(expected, got) {
-		t.Errorf("expected %d, got %d", expected, got)
-	}
+		if !reflect.DeepEqual(expected, got) {
+			t.Errorf("expected %d, got %d", expected, got)
+		}
+	})
+
+	t.Run("it handles empty slices", func(t *testing.T) {
+		expected := []int{2, 0}
+		got := SumAllTails([]int{1, 2}, []int{})
+
+		if !reflect.DeepEqual(expected, got) {
+			t.Errorf("expected %d, got %d", expected, got)
+		}
+	})
 }
