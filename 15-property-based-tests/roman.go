dc2c7d47c446a95a5a0865125877ba7bfdfae391
Marc Kelsey <marc.kelsey@gmail.com>
2021-12-04 10:48:31 -0500

    convert to roman done

---
+++ 15-property-based-tests/roman.go
@@ -0,0 +1,37 @@
+package roman
+
+import "strings"
+
+type RomanNumber struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumbers = []RomanNumber{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic int) string {
+	var result strings.Builder
+
+	for _, rn := range allRomanNumbers {
+		for arabic >= rn.Value {
+			result.WriteString(rn.Symbol)
+			arabic -= rn.Value
+		}
+	}
+
+	return result.String()
+}
---
+++ 15-property-based-tests/roman_test.go
@@ -0,0 +1,51 @@
+package roman
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T) {
+	tests := []struct {
+		arabic int
+		roman  string
+	}{
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{7, "VII"},
+		{8, "VIII"},
+		{9, "IX"},
+		{10, "X"},
+		{14, "XIV"},
+		{18, "XVIII"},
+		{20, "XX"},
+		{39, "XXXIX"},
+		{40, "XL"},
+		{44, "XLIV"},
+		{47, "XLVII"},
+		{49, "XLIX"},
+		{50, "L"},
+		{89, "LXXXIX"},
+		{400, "CD"},
+		{500, "D"},
+		{900, "CM"},
+		{1000, "M"},
+		{1984, "MCMLXXXIV"},
+		{3999, "MMMCMXCIX"},
+		{2014, "MMXIV"},
+		{1006, "MVI"},
+		{798, "DCCXCVIII"},
+	}
+
+	for _, test := range tests {
+		t.Run(`$test.arabic => $test.roman`, func(t *testing.T) {
+			got := ConvertToRoman(test.arabic)
+			expected := test.roman
+
+			if got != expected {
+				t.Errorf("expected %s, got %s", expected, got)
+			}
+		})
+	}
+}
