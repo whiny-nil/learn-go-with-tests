51eb7a808e0c806c11303f4ad7cdf4f1d303a047
Marc Kelsey <marc.kelsey@textnow.com>
2022-11-01 16:02:54 -0400

    Finish generics and functional programming :/

--- 20-generic-arrays-slices/sums/sums.go
+++ 20-generic-arrays-slices/sums/sums.go
@@ -30,7 +30,7 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	return Reduce(numbersToSum, adder, sums)
 }
 
-func Reduce[T any](collection []T, accumulator func(T, T) T, initialValue T) T {
+func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
 	value := initialValue
 	for _, nextValue := range collection {
 		value = accumulator(value, nextValue)
@@ -39,22 +39,45 @@ func Reduce[T any](collection []T, accumulator func(T, T) T, initialValue T) T {
 	return value
 }
 
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, item := range items {
+		if predicate(item) {
+			return item, true
+		}
+	}
+
+	return
+}
+
 type Transaction struct {
 	From string
 	To   string
 	Sum  float64
 }
 
-func BalanceFor(transactions []Transaction, name string) float64 {
-	var balance float64
-	for _, t := range transactions {
-		if t.From == name {
-			balance -= t.Sum
-		}
-		if t.To == name {
-			balance += t.Sum
-		}
-	}
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
 
-	return balance
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+	return a
 }
--- 20-generic-arrays-slices/sums/sums_test.go
+++ 20-generic-arrays-slices/sums/sums_test.go
@@ -64,22 +64,55 @@ func TestReduce(t *testing.T) {
 }
 
 func TestBadBank(t *testing.T) {
-	transactions := []Transaction{
-		{
-			From: "Chris",
-			To:   "Riya",
-			Sum:  100,
-		},
-		{
-			From: "Adil",
-			To:   "Chris",
-			Sum:  25,
-		},
+	var (
+		riya  = Account{Name: "Riya", Balance: 100}
+		chris = Account{Name: "Chris", Balance: 75}
+		adil  = Account{Name: "Adil", Balance: 200}
+
+		transactions = []Transaction{
+			NewTransaction(chris, riya, 100),
+			NewTransaction(adil, chris, 25),
+		}
+	)
+
+	newBalanceFor := func(account Account) float64 {
+		return NewBalanceFor(account, transactions).Balance
 	}
 
-	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
-	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
-	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+	AssertEqual(t, newBalanceFor(riya), 200)
+	AssertEqual(t, newBalanceFor(chris), 0)
+	AssertEqual(t, newBalanceFor(adil), 175)
+}
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, firstEvenNumber, 2)
+	})
+
+	t.Run("find best programmer", func(t *testing.T) {
+		type Programmer struct {
+			Name string
+		}
+
+		programmers := []Programmer{
+			{Name: "Bill"},
+			{Name: "Marc"},
+		}
+
+		bestProgrammer, found := Find(programmers, func(x Programmer) bool {
+			return x.Name == "Marc"
+		})
+
+		AssertTrue(t, found)
+		AssertEqual(t, bestProgrammer, Programmer{Name: "Marc"})
+	})
 }
 
 func AssertEqual[T comparable](t *testing.T, got, want T) {
