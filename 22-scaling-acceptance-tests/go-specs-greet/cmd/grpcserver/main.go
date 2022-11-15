5e0b9f49cdeaa0c1ad6a6bf4c5da36f9ed9a8a84
Marc Kelsey <marc.kelsey@textnow.com>
2022-11-15 15:10:17 -0500

    Refactor grpc

--- 22-scaling-acceptance-tests/go-specs-greet/cmd/grpcserver/main.go
+++ 22-scaling-acceptance-tests/go-specs-greet/cmd/grpcserver/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
 	"github.com/whiny-nil/learn-go-with-tests/22-scaling-acceptance-tests/go-specs-greet/adapters/grpcserver"
-	go_specs_greet "github.com/whiny-nil/learn-go-with-tests/22-scaling-acceptance-tests/go-specs-greet/domain/interactions"
 
 	"google.golang.org/grpc"
 )
@@ -18,17 +16,9 @@ func main() {
 	}
 
 	s := grpc.NewServer()
-	grpcserver.RegisterGreeterServer(s, &GreetServer{})
+	grpcserver.RegisterGreeterServer(s, &grpcserver.GreetServer{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type GreetServer struct {
-	grpcserver.UnimplementedGreeterServer
-}
-
-func (g GreetServer) Greet(ctx context.Context, request *grpcserver.GreetRequest) (*grpcserver.GreetReply, error) {
-	return &grpcserver.GreetReply{Message: go_specs_greet.Greet(request.Name)}, nil
 }
