31a38e1a85d0cce76f21dbdd64a4f95d0a69466c
Marc Kelsey <marc.kelsey@textnow.com>
2022-11-29 10:41:25 -0500

    Refactor player store

--- 23-build-an-application/main.go
+++ 23-build-an-application/main.go
@@ -14,7 +14,7 @@ func main() {
 		log.Fatalf("Problem opening %s %v", dbFileName, err)
 	}
 
-	store := &FileSystemPlayerStore{db}
+	store := NewFileSystemPlayerStore(db)
 	server := NewPlayerServer(store)
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
